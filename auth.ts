820c9802a221a9b3db151a341915df51d1c5031a
Alan gomez <alangomez0047@gmail.com>
2025-08-31 20:46:18 +0530

    prisma-adapter compatability issue solve

--- auth.ts
+++ auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialProvider from "next-auth/providers/credentials";
 import { checkUser, createUser, credentialCheck } from "./app/queries/auth";

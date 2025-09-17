20d93bf28a95eb9748e916ad0226e35dfcd5bea5
Alan gomez <alangomez0047@gmail.com>
2025-09-17 15:48:02 +0530

    route name fixed

--- components/cards/review-card.tsx
+++ components/cards/review-card.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Star } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
